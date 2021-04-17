49260129cffeecc5e662a0679daf7eb3233525fa
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-04-17 07:30:41 +0700

    Ref | Composition API Vue 3

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{message}}</div>
+</template>
+
+<script>
+import {ref} from "vue";
+export default {
+  setup(){
+    const message = ref('hello');
+
+    setTimeout(() => {
+      message.value ='composition api';
+    },2000);
+
+    return {
+      message
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
