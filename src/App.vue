d606a100d6048053983b873cc625cbbe459e5737
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-04-17 07:55:31 +0700

    Computed | Composition API Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div>Count : {{ nilai }}</div>
   <button @click="add">Add</button>
+  <div>Result : {{ result }}</div>
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
+import { reactive, ref, toRefs, computed } from "vue";
 export default {
   setup() {
     const counter = reactive({
-      nilai: 0
+      nilai: 0,
     });
+    const addNum = ref(2);
 
     const add = () => {
       counter.nilai++;
     };
 
+    const result = computed(() => 
+      counter.nilai + addNum.value
+    );
+
     return {
-     ...toRefs(counter),
+      ...toRefs(counter),
       add,
+      result,
     };
   },
 };
