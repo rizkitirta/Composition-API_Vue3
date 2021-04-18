e611057273b875214752ddf9d666654eed282a30
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-04-18 13:41:09 +0700

    Watch | Composition API Vue 3

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,37 @@
 </template>
 
 <script>
-import { reactive, ref, toRefs, computed, watchEffect } from "vue";
+import { reactive, ref, toRefs, computed, watch } from "vue";
 export default {
   setup() {
     const counter = reactive({
       nilai: 0,
     });
 
+    const num1 = ref(1);
+    const num2 = ref(2);
+
     const add = () => {
-      counter.nilai++;
+      // counter.nilai++;
+      num1.value++;
+      num2.value++;
     };
 
-    watchEffect(() => {
-      console.log(counter.nilai)
-    })
+    watch([num1, num2], (after, before) => {
+      console.log(after,before);
+      // console.log(num2.value);
+      // console.log(after);
+      // console.log(before);
+    });
+
+    // watch(
+    //   () => counter.nilai,
+    //   (current, before) => {
+    //     console.log(counter.nilai);
+    //     console.log(current);
+    //     console.log(before);
+    //   }
+    // );
 
     return {
       ...toRefs(counter),
