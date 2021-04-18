977166c6738285f3171681e32c0f051a406b5583
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-04-18 15:10:19 +0700

    Todo List | Composition API Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,81 @@
 <template>
-  <user-component
-    class="active"
-    :label="label"
-    :user="user"
-    blabla="testing"
-    @submit="onSubmit"
-  >
-    <div>Ini Slot</div>
-  </user-component>
-  <button @click="changeName">Change</button>
+  <div class="container" style="margin-top: 20px">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Simple Todo App</h5>
+        <div class="row">
+          <div class="col-10">
+            <input
+              v-model="todo"
+              type="text"
+              class="form-control"
+              @keyup.enter="add"
+            />
+          </div>
+          <div class="col-auto">
+            <button class="btn btn-success" @click="add">Add</button>
+          </div>
+        </div>
+        <list :todos="list" @deleteTodo="deleteTodo" @doneTodo="doneTodo" />
+        <small>Total Todo : {{ totalTODO }}</small>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
-import UserComponent from "./components/UserComponent.vue";
+import { ref, reactive, onMounted, computed, toRefs } from "vue";
+import List from "./components/List.vue";
 export default {
-  components: { UserComponent },
+  components: { List },
   setup() {
-    const label = ref("Biodata");
-    const user = reactive({
-      name: "Rizki",
-      age: 25,
+    const todo = ref("");
+    const todos = reactive({
+      list: [],
     });
 
-    const changeName = () => {
-      user.name = "Admin Kedua";
+    onMounted(() => {
+      todos.list = JSON.parse(localStorage.getItem("todos"));
+    });
+
+    const totalTODO = computed(() => {
+      return todos.list.length;
+    });
+
+    const add = () => {
+      todos.list.unshift({
+        activity: todo.value,
+        isDone: false,
+      });
+      todo.value = "";
+      saveToLocalStorage();
     };
 
-    const onSubmit = (val) => {
-      console.log(val);
+    const deleteTodo = (todoIndex) => {
+      todos.list = todos.list.filter((item, index) => {
+        if (index != todoIndex) {
+          return item;
+        }
+      });
+      saveToLocalStorage();
     };
 
-    return {
-      label,
-      user,
-      changeName,
-      onSubmit,
+    const doneTodo = (todoIndex) => {
+      todos.list = todos.list.filter((item, index) => {
+        if (index == todoIndex) {
+          item.isDone = true;
+        }
+
+        return item;
+      });
+      saveToLocalStorage();
     };
+
+    const saveToLocalStorage = () => {
+      localStorage.setItem("todos", JSON.stringify(todos.list));
+    };
+
+    return { todo, ...toRefs(todos), totalTODO, add, deleteTodo, doneTodo };
   },
 };
 </script>
---
+++ src/components/List.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="mt-2">
+    <ul class="list-group">
+      <li class="list-group-item" v-for="(todo, index) in todos" :key="index">
+        <div class="row">
+          <div class="col">
+            <span v-if="todo.isDone">
+              <del>{{ todo.activity }}</del>
+            </span>
+            <span v-else>{{ todo.activity }}</span>
+          </div>
+          <div class="col-auto">
+            <div class="row gx-2">
+              <div class="col">
+                <button @click="done(index)" class="btn btn-info">Done</button>
+              </div>
+              <div class="col">
+                <button @click="deleteTODO(index)" class="btn btn-danger">
+                  X
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      default: [],
+    },
+  },
+  setup(props, { emit }) {
+    const deleteTODO = (index) => {
+      emit("deleteTodo", index);
+    };
+
+    const done = (index) => {
+      emit("doneTodo", index);
+    };
+
+    return { deleteTODO, done}
+  },
+};
+</script>
+
+<style>
+</style>
