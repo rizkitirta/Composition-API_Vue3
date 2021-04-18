b6fe5d7f748744078636d81f937ec706ab3a691a
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-04-18 13:47:38 +0700

    Lifecycle | Composition API Vue 3

--- src/App.vue
+++ src/App.vue
@@ -4,43 +4,23 @@
 </template>
 
 <script>
-import { reactive, ref, toRefs, computed, watch } from "vue";
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  onActivated,
+  onDeactivated,
+  onErrorCaptured,
+} from "vue";
 export default {
   setup() {
-    const counter = reactive({
-      nilai: 0,
-    });
-
-    const num1 = ref(1);
-    const num2 = ref(2);
-
-    const add = () => {
-      // counter.nilai++;
-      num1.value++;
-      num2.value++;
-    };
-
-    watch([num1, num2], (after, before) => {
-      console.log(after,before);
-      // console.log(num2.value);
-      // console.log(after);
-      // console.log(before);
-    });
-
-    // watch(
-    //   () => counter.nilai,
-    //   (current, before) => {
-    //     console.log(counter.nilai);
-    //     console.log(current);
-    //     console.log(before);
-    //   }
-    // );
-
     return {
-      ...toRefs(counter),
-      add,
-    };
-  },
+
+    }
+  }
 };
 </script>
 
